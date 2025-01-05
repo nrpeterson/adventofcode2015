6562376a0a2af641a5eb6ab7938b7a1b97edca35
Nick Peterson <nrpeterson@gmail.com>
2025-01-04 20:55:52 -0800

    I think that's a wrap for day 19. :-)

--- src/bin/day19.rs
+++ src/bin/day19.rs
@@ -2,33 +2,33 @@ use std::collections::{HashMap, HashSet};
 use adventofcode2015::build_main;
 
 #[derive(Debug, Eq, PartialEq, Clone)]
-struct Rule {
-    from: String,
-    to: Vec<String>
+struct Rule<'a> {
+    from: &'a str,
+    to: Vec<&'a str>
 }
 
-struct Grammar {
-    start_rule: Rule,
-    rules: HashMap<String, Vec<Rule>>
+struct Grammar<'a> {
+    pub rules: Vec<Rule<'a>>,
+    pub start_rule_id: usize,
+    pub rule_ids_by_from: HashMap<&'a str, Vec<usize>>
 }
 
-impl Grammar {
-    fn new(start_rule: Rule, rules_list: Vec<Rule>) -> Grammar {
-        let mut rules: HashMap<String, Vec<Rule>> = HashMap::new();
-        rules_list.into_iter().for_each(|rule| {
-            rules.entry(rule.from.clone())
-                .or_default()
-                .push(rule);
-        });
+impl<'a> Grammar<'a> {
+    fn new(start_rule_id: usize, rules: Vec<Rule<'a>>) -> Grammar<'a> {
+        let mut rule_ids_by_from: HashMap<&'a str, Vec<usize>> = HashMap::new();
+        for (id, rule) in rules.iter().enumerate() {
+            let from = rule.from;
+            rule_ids_by_from.entry(from).or_default().push(id);
+        }
 
-        Grammar { start_rule, rules }
+        Grammar { start_rule_id, rules , rule_ids_by_from }
     }
 }
 
 mod earley {
     use std::ops::Index;
     use itertools::{chain, Itertools};
-    use crate::{Grammar, Rule};
+    use crate::Grammar;
 
     #[derive(Copy, Clone)]
     struct BackPointer {
@@ -36,21 +36,11 @@ mod earley {
         advanced_by: Option<(usize, usize)>
     }
 
-    #[derive(Debug, Eq, PartialEq, Clone)]
+    #[derive(Debug, Eq, PartialEq, Clone, Copy)]
     struct EarleyItem {
-        rule: Rule,
+        rule_id: usize,
         rule_state: usize,
         parse_start: usize
-    }
-
-    impl EarleyItem {
-        fn is_complete(&self) -> bool {
-            self.rule_state == self.rule.to.len()
-        }
-
-        fn cur_symb(&self) -> Option<&String> {
-            self.rule.to.get(self.rule_state)
-        }
     }
 
     struct ParseTable {
@@ -63,6 +53,111 @@ mod earley {
             let data = vec![Vec::new(); n];
             let completed_start_rule = None;
             ParseTable { data, completed_start_rule }
+        }
+
+        fn build(grammar: &Grammar, target: &[&str]) -> ParseTable {
+            let mut table: ParseTable = ParseTable::with_rows(target.len() + 1);
+
+            table.insert(
+                EarleyItem { rule_id: grammar.start_rule_id, rule_state: 0, parse_start: 0 },
+                0,
+                None
+            );
+
+            for cur_parse_end in 0..=target.len() {
+                let mut cur_item_index = 0;
+                while cur_item_index < table[cur_parse_end].len() {
+                    let cur_item = table[cur_parse_end][cur_item_index].0;
+                    let cur_rule = &grammar.rules[cur_item.rule_id];
+
+                    if cur_item.rule_state == cur_rule.to.len() {
+                        // We've completed this item. If any items were waiting for this, we can
+                        // advance them one step.
+                        let mut j = 0;
+                        while j < table[cur_item.parse_start].len() {
+                            let next_item = &table[cur_item.parse_start][j].0;
+                            let next_rule = &grammar.rules[next_item.rule_id];
+                            let next_is_complete = next_item.rule_state == next_rule.to.len();
+
+                            if !next_is_complete &&
+                                next_rule.to[next_item.rule_state] == cur_rule.from {
+                                let new_item = EarleyItem {
+                                    rule_id: next_item.rule_id,
+                                    rule_state: next_item.rule_state + 1,
+                                    parse_start: next_item.parse_start
+                                };
+
+                                table.insert(
+                                    new_item,
+                                    cur_parse_end,
+                                    Some(BackPointer {
+                                        prev_item: (cur_item.parse_start, j),
+                                        advanced_by: Some((cur_parse_end, cur_item_index))
+                                    })
+                                );
+                            }
+
+                            j += 1;
+                        }
+                    }
+                    else {
+                        let cur_item = &table[cur_parse_end][cur_item_index].0;
+                        let cur_rule = &grammar.rules[cur_item.rule_id];
+                        let cur_rule_symb = &cur_rule.to[cur_item.rule_state];
+
+                        // Scan: if the current symbol of target matches the current symbol of our
+                        // current rule, we can "consume" it and advance this rule by one.
+                        //
+                        // Note that unlike the traditional Earley parser, I've allowed this to
+                        // happen whether or not the current symbol is terminal.  That way, we can
+                        // query against nonterminal symbols instead of having to modify the
+                        // grammar.
+                        if cur_parse_end < target.len() && *cur_rule_symb == target[cur_parse_end] {
+                            table.insert(
+                                EarleyItem {
+                                    rule_id: cur_item.rule_id,
+                                    rule_state: cur_item.rule_state + 1,
+                                    parse_start: cur_item.parse_start },
+                                cur_parse_end + 1,
+                                Some(BackPointer {
+                                    prev_item: (cur_parse_end, cur_item_index),
+                                    advanced_by: None }
+                                )
+                            );
+                        }
+
+                        // Predict: we may want to use another rule whose "from" is our current
+                        // rule's current symbol, so insert them at this point to give it a try.
+                        if let Some(rule_ids) = grammar.rule_ids_by_from.get(cur_rule_symb) {
+                            for &rule_id in rule_ids {
+                                table.insert(
+                                    EarleyItem {
+                                        rule_id,
+                                        rule_state: 0,
+                                        parse_start: cur_parse_end
+                                    },
+                                    cur_parse_end,
+                                    None
+                                );
+                            }
+                        }
+                    }
+
+                    cur_item_index += 1;
+                }
+            }
+
+            let k = target.len();
+            let completed_start = table[k].iter().enumerate()
+                .find(|(_, (item, _))| {
+                    let rule = &grammar.rules[item.rule_id];
+                    item.rule_id == grammar.start_rule_id && item.rule_state == rule.to.len()
+                })
+                .map(|(i, _)| (k, i));
+
+            table.completed_start_rule = completed_start;
+
+            table
         }
 
         fn insert(&mut self, item: EarleyItem, parse_end: usize, pred: Option<BackPointer>) {
@@ -90,124 +185,13 @@ mod earley {
         }
     }
 
-    fn build_parse_table<'a>(grammar: &Grammar, target: &[String]) -> ParseTable {
-        let mut table: ParseTable = ParseTable::with_rows(target.len() + 1);
-
-        table.insert(
-            EarleyItem { rule: grammar.start_rule.clone(), rule_state: 0, parse_start: 0 },
-            0,
-            None
-        );
-
-        for cur_parse_end in 0..=target.len() {
-            let mut cur_item_index = 0;
-            while cur_item_index < table[cur_parse_end].len() {
-                let cur_symb = table[cur_parse_end][cur_item_index].0
-                    .cur_symb()
-                    .map(|symb| symb.clone());
-
-
-                if cur_symb.is_none() {
-                    // We've completed this item. If any items were waiting for this, we can
-                    // advance them one step.
-                    let cur_item  = &table[cur_parse_end][cur_item_index].0;
-                    let cur_parse_start = cur_item.parse_start;
-                    let cur_rule_from = cur_item.rule.from.clone();
-
-                    let mut j = 0;
-                    while j < table[cur_parse_start].len() {
-                        let next_item = &table[cur_parse_start][j].0;
-
-                        if !next_item.is_complete() &&
-                            next_item.rule.to[next_item.rule_state] == cur_rule_from {
-                            let new_item = EarleyItem {
-                                rule: next_item.rule.clone(),
-                                rule_state: next_item.rule_state + 1,
-                                parse_start: next_item.parse_start
-                            };
-
-                            table.insert(
-                                new_item,
-                                cur_parse_end,
-                                Some(BackPointer {
-                                    prev_item: (cur_parse_start, j),
-                                    advanced_by: Some((cur_parse_end, cur_item_index))
-                                })
-                            );
-                        }
-
-                        j += 1;
-                    }
-                }
-                else {
-                    let cur_item = &table[cur_parse_end][cur_item_index].0;
-                    let cur_rule_symb = cur_item.cur_symb().unwrap().clone();
-
-                    // Scan: if the current symbol of target matches the current symbol of our
-                    // current rule, we can "consume" it and advance this rule by one.
-                    //
-                    // Note that unlike the traditional Earley parser, I've allowed this to
-                    // happen whether or not the current symbol is terminal.  That way, we can
-                    // query against nonterminal symbols instead of having to modify the
-                    // grammar.
-                    if cur_parse_end < target.len() && *cur_rule_symb == target[cur_parse_end] {
-                        table.insert(
-                            EarleyItem {
-                                rule: cur_item.rule.clone(),
-                                rule_state: cur_item.rule_state + 1,
-                                parse_start: cur_item.parse_start },
-                            cur_parse_end + 1,
-                            Some(BackPointer {
-                                prev_item: (cur_parse_end, cur_item_index),
-                                advanced_by: None }
-                            )
-                        );
-                    }
-
-                    // Predict: we may want to use another rule whose "from" is our current
-                    // rule's current symbol, so insert them at this point to give it a try.
-                    if grammar.rules.contains_key(&cur_rule_symb) {
-                        for rule in grammar.rules[&cur_rule_symb].iter() {
-                            table.insert(
-                                EarleyItem {
-                                    rule: rule.clone(),
-                                    rule_state: 0,
-                                    parse_start: cur_parse_end
-                                },
-                                cur_parse_end,
-                                None
-                            );
-                        }
-                    }
-                }
-
-                cur_item_index += 1;
-            }
-        }
-
-        let k = target.len();
-        let completed_start = table[k].iter().enumerate()
-            .find(|(_, (item, _))| item.is_complete() && item.rule == grammar.start_rule)
-            .map(|(i, _)| (k, i));
-
-        table.completed_start_rule = completed_start;
-
-        table
-    }
-
-    pub fn min_parse_tree(grammar: &Grammar, target: &[String]) -> usize {
-        let table = build_parse_table(grammar, target);
+    pub fn min_rules(grammar: &Grammar, target: &[&str]) -> Option<usize> {
+        let table = ParseTable::build(grammar, target);
         let mut cache: Vec<Vec<Option<usize>>> = table.iter().map(
             |s| s.iter().map(|_| None).collect()
         ).collect();
 
-        let k0 = target.len();
-        let i0 = table[k0].iter().enumerate()
-            .find(|(_, (item, _))| {
-                item.rule == grammar.start_rule && item.is_complete()
-            })
-            .map(|(i, _)| i)
-            .expect("No solutions found");
+        let (k0, i0) = table.completed_start_rule?;
 
         let mut stack = vec![(k0, i0)];
 
@@ -249,7 +233,7 @@ mod earley {
             }
         }
 
-        cache[k0][i0].unwrap()
+        Some(cache[k0][i0].unwrap())
     }
 }
 
@@ -271,14 +255,11 @@ mod parse {
         one_of("abcdefghijklmnopqrstuvwxyz")(input)
     }
 
-    fn symbol(input: &str) -> IResult<&str, String> {
-        map(
-            alt((
-                recognize(pair(upper_char, many0(lower_char))),
-                recognize(character('e'))
-            )),
-            |s: &str| s.to_owned()
-        )(input)
+    fn symbol(input: &str) -> IResult<&str, &str> {
+        alt((
+            recognize(pair(upper_char, many0(lower_char))),
+            recognize(character('e'))
+        ))(input)
     }
 
     fn rule(input: &str) -> IResult<&str, Rule> {
@@ -295,14 +276,15 @@ mod parse {
     fn grammar(input: &str) -> IResult<&str, Grammar> {
         map(
             separated_list1(newline, rule),
-            |rules| {
-                let start_rule = Rule { from: "S0".to_owned(), to: vec!["e".to_owned()] };
-                Grammar::new(start_rule, rules)
+            |mut rules| {
+                let start_rule = Rule { from: "S0", to: vec!["e"] };
+                rules.push(start_rule);
+                Grammar::new(rules.len() - 1, rules)
             }
         )(input)
     }
 
-    pub fn input(input: &str) -> IResult<&str, (Grammar, Vec<String>)> {
+    pub fn input(input: &str) -> IResult<&str, (Grammar, Vec<&str>)> {
         separated_pair(grammar, multispace1, many1(symbol))(input)
     }
 }
@@ -312,16 +294,15 @@ fn part1(input: &str) -> usize {
 
     let mut seen = HashSet::new();
 
-    for (_, rules) in grammar.rules {
-        for rule in rules {
-            for i in 0..target.len() {
-                if target[i] == rule.from {
-                    let mut new: Vec<String> = Vec::new();
-                    new.extend(target[..i].iter().cloned());
-                    new.extend(rule.to.iter().cloned());
-                    new.extend(target[i+1..].iter().cloned());
-                    seen.insert(new);
-                }
+    for rule in grammar.rules {
+
+        for i in 0..target.len() {
+            if target[i] == rule.from {
+                let mut new: Vec<&str> = Vec::new();
+                new.extend(target[..i].iter());
+                new.extend(rule.to.iter());
+                new.extend(target[i+1..].iter());
+                seen.insert(new);
             }
         }
     }
@@ -331,7 +312,7 @@ fn part1(input: &str) -> usize {
 fn part2(input: &str) -> usize {
     let (grammar, target) = parse::input(input).unwrap().1;
 
-    earley::min_parse_tree(&grammar, &target) - 1
+    earley::min_rules(&grammar, &target).unwrap() - 1
 }
 
 
@@ -341,7 +322,15 @@ build_main!("day19.txt", "Part 1" => part1, "Part 2" => part2);
 mod tests {
     use super::*;
 
-    const TEST_INPUT: &str = "e => H
+    const TEST_INPUT1: &str = "e => H
+e => O
+H => HO
+H => OH
+O => HH
+
+HOH";
+
+    const TEST_INPUT2: &str = "e => H
 e => O
 H => HO
 H => OH
@@ -350,7 +339,14 @@ O => HH
 HOHOHO";
 
     #[test]
+    fn test_part1() {
+        assert_eq!(part1(TEST_INPUT1), 4);
+        assert_eq!(part1(TEST_INPUT2), 7);
+    }
+
+    #[test]
     fn test_part2() {
-        assert_eq!(part2(TEST_INPUT), 6);
+        assert_eq!(part2(TEST_INPUT1), 3);
+        assert_eq!(part2(TEST_INPUT2), 6);
     }
 }
